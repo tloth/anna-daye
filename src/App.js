6508d1d159d4140aa1f6aa0c14b378e5ab9ca735
tloth <toth.annar@gmail.com>
2019-09-08 14:21:15 +0100

    add theme.js stylesheet with imported fonts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Daye
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
---
+++ src/theme.js
@@ -0,0 +1,34 @@
+// styles to be imported into styled components
+
+export const fontFamily = {
+    titleFont: 'Abril Fatface, cursive',
+    baseFont: 'Open Sans, sans-serif'
+};
+
+export const fontSize = {
+    big: '3em',
+    medium: '2em',
+    small: '1.2em',
+    tiny: '1em'
+};
+
+export const colours = {
+    baseColour: '#FEFEE5',
+    darkGreen: '#004234',
+    lightGreen: '#BDECBD',
+    darkYellow: 'F2A900',
+    accentColour: '#FFA7A3'
+};
+
+export const borders = {
+    darkGreen: '1px solid ${colours.darkGreen}',
+    darkYellow: '1px solid ${colours.darkYellow}',
+    dashed: '1px dashed ${colours.darkGreen}'
+};
+
+export const spacing = {
+    big: '3em',
+    medium: '2em',
+    small: '1.2em',
+    tiny: '0.5em'
+}
