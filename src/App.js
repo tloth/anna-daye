8588ac39f0a075ae8b5166f85ebb2119bbd0ab13
tloth <toth.annar@gmail.com>
2019-09-11 14:55:29 +0100

    get rid of trash rip spinning d

--- src/App.js
+++ src/App.js
@@ -27,20 +27,6 @@ function App() {
       <Header />
       <Slider />
       <Table />
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Daye
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </AppContainer>
   );
 }
